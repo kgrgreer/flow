b0e9795a02f931cf46f55ed9990ea8ac015cb6f3
Kevin Greer <kgrgreer@gmail.com>
2024-12-23 15:26:55 -0500

    Cleanup POM and generate java for Property.

--- src/com/google/flow/pom.js
+++ src/com/google/flow/pom.js
@@ -1,16 +1,16 @@
 foam.POM({
   name: 'flow',
   files: [
-    { name: 'Calc', flags: 'js' },
-    { name: "Canvas",                                      flags: "js" },
-    { name: "Select",                                      flags: "js" },
-    { name: "TreeView",                                    flags: "js" },
-    { name: "Property",                                    flags: "js" },
-    { name: "FLOW",                                        flags: "js|java" },
-    { name: "FLOWController",                              flags: "js" },
-    { name: 'Halo', flags: 'js' },
-    { name: 'LineHalo', flags: 'js' },
-    { name: 'ReactiveDetailView', flags: 'js' },
-    { name: 'Things', flags: 'js' }
+    { name: 'Calc',                   flags: 'js' },
+    { name: "Canvas",                 flags: "js" },
+    { name: "Select",                 flags: "js" },
+    { name: "TreeView",               flags: "js" },
+    { name: "Property",               flags: "js|java" },
+    { name: "FLOW",                   flags: "js|java" },
+    { name: "FLOWController",         flags: "js" },
+    { name: 'Halo',                   flags: 'js' },
+    { name: 'LineHalo',               flags: 'js' },
+    { name: 'ReactiveDetailView',     flags: 'js' },
+    { name: 'Things',                 flags: 'js' }
   ]
 });
