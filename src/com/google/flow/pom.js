0d811112242f26f35967aa08afffcdabdeffaa57
Kevin Greer <kgrgreer@gmail.com>
2024-12-23 15:23:52 -0500

    Update POM to reflect POM.js being split into separate files.

--- src/com/google/flow/pom.js
+++ src/com/google/flow/pom.js
@@ -2,7 +2,12 @@ foam.POM({
   name: 'flow',
   files: [
     { name: 'Calc', flags: 'js' },
-    { name: 'FLOW', flags: 'js' },
+    { name: "Canvas",                                      flags: "js" },
+    { name: "Select",                                      flags: "js" },
+    { name: "TreeView",                                    flags: "js" },
+    { name: "Property",                                    flags: "js" },
+    { name: "FLOW",                                        flags: "js|java" },
+    { name: "FLOWController",                              flags: "js" },
     { name: 'Halo', flags: 'js' },
     { name: 'LineHalo', flags: 'js' },
     { name: 'ReactiveDetailView', flags: 'js' },
