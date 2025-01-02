4e408f24b7840efa75078a3c03d63481b9a16c8d
Kevin Greer <kgrgreer@gmail.com>
2025-01-02 08:55:56 -0500

    Fix syntax error.

--- src/com/google/flow/Console.js
+++ src/com/google/flow/Console.js
@@ -89,8 +89,8 @@ foam.CLASS({
           style({display: 'inline-flex', float: 'left'}).
         start('b').style({'margin-top': '6px', 'margin-right': '4px', display: 'flex', 'white-space': 'pre'}).call(function() { self.outputLink('help', () => self.eval_('help'), this); }).add(' >').end().
           start(this.INPUT, null, this.input_$).focus().addClass(this.myClass('input')).end().
-        end().
-        /* on('click', this.onClick) causes issues with embedded views that need focus */;
+        end();
+        /* on('click', this.onClick) causes issues with embedded views that need focus */
 
        this.input$.sub(this.onInput);
     },
