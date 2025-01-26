5a4ade41245d52cf4c5a1e5ce5a4b5643e4256dd
Kevin Greer <kgrgreer@gmail.com>
2025-01-26 17:53:58 -0400

    Add needed FOAM classes to POM.

--- pom.js
+++ pom.js
@@ -40,6 +40,9 @@ foam.POM({
     { name: 'foam3/src/foam/demos/robot/Robot',     flags: 'js' },
     { name: 'foam3/src/foam/demos/sevenguis/Cells', flags: 'js' },
     { name: 'foam3/src/foam/nanos/zac/Client',      flags: 'js' },
-    { name: 'foam3/src/foam/demos/olympics/Medal',  flags: 'js|java' }
+    { name: 'foam3/src/foam/demos/olympics/Medal',  flags: 'js|java' },
+    { name: 'foam3/src/foam/u2/mlang/Pie',          flags: 'js|java' },
+    { name: 'foam3/src/foam/graphics/DataSource',   flags: 'js' },
+    { name: 'foam3/src/foam/graphics/PieGraph',     flags: 'js' }
   ]
 });
